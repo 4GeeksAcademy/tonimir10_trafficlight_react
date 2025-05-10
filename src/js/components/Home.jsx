b6f975baae24d7d0d0d1fad655f41fb0fdaad68b
Tonimir10 <91009470+Tonimir10@users.noreply.github.com>
2025-05-10 11:14:23 +0000

    useState y useEffect

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,25 +1,42 @@
-import React from "react";
-import './protector.css'
-import Protector from './Protector'
-import Light from "./Light";
-import './light.css'
-import Button from "./Button";
+import React, { useEffect, useState } from "react";
 //include images into your bundle
 //import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+  const [selected, setSelected] = useState('red');
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+   if(active){
+      setInterval(() => {
+        
+        if(selected == 'red'){
+          setSelected('yellow');
+        }
+        if(selected == 'yellow'){
+          setSelected('green');
+        }
+        if(selected == 'green'){
+          setSelected('red');
+        }
+      },3000)
+      }
+    
+  },[active, selected])
+  
 	return (
+    <>
   <div className="trafficlight">
-   <Protector/>
-   <Protector/>
-   <Protector/>
-   <Light color='red'/>
-  <Light color='yellow'/>
-  <Light color='green'/>
-  <Button/>
-</div>
-
+    <div className="p-box"></div>
+    <div className="box-light">
+      <div className={`red ${selected == 'red' ? 'active' : ''}`}></div>
+      <div className={`yellow ${selected == 'yellow' ? 'active' : ''}`} ></div>
+      <div className={`green ${selected == 'green' ? 'active' : ''}`} ></div>
+    </div>
+    <button className="btn btn-success" onClick={() => setActive(true)}>Activar</button>
+  </div>
+  
+</>
 	);
 };
 
